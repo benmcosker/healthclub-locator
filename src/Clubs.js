a8a8e5444789ee323b646c65c3e4fd41bfec0b84
Ben Mcosker <benmcosker@gmail.com>
2017-05-24 20:58:24 -0400

    text color

--- src/Clubs.js
+++ src/Clubs.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import {Card, CardTitle, CardText} from 'material-ui/Card';
-import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+injectTapEventPlugin();
 
 export default ({info}) => (
-    <Card className="club-card">
-        <CardTitle title={info.name} className="title-spacing"/>
-        <CardText className="title-margin">
-            <p>{info.street}</p>
-            <p>{info.city} {info.state}, {info.zip}</p>
-            <p className="chain">
-                <strong>{info.is_chain.toString() === '1' ? 'Gym is part of a chain' : '' }</strong>
-            </p>
-            <p><a href={`https://www.google.com/maps/search/?api=1&query=${info.lat},${info.lon}`} target="_blank">Google Maps</a></p>
-            <RaisedButton label="Google Map Location" className="button-margin" />
-        </CardText>
-    </Card>
+    <MuiThemeProvider>
+        <Card className="club-card">
+            <CardTitle title={info.name} className="title-spacing"/>
+            <CardText className="title-margin">
+                <p>{info.street}</p>
+                <p>{info.city} {info.state}, {info.zip}</p>
+                <p className="chain">
+                    <strong>{info.is_chain.toString() === '1' ? 'Gym is part of a chain' : '' }</strong>
+                </p>
+                <p className="google-map"><a href={`https://www.google.com/maps/search/?api=1&query=${info.lat},${info.lon}`} target="_blank">Google Maps</a></p>
+            </CardText>
+        </Card>
+    </MuiThemeProvider>
 )
